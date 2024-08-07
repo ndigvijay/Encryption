8840062ba878293f5b94a972040d5dded80d3cbe
Nishanth <nishanthdmello2003@gmail.com>
2024-08-07 20:38:19 +0530

    added server

--- backend/server.py
+++ backend/server.py
@@ -1,2 +1,10 @@
-print("hello world")
-print("heloo nishanth")
+from flask import Flask, request, jsonify
+
+app=Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return jsonify({"message":"home page"}),200
+
+if __name__=="__main__":
+    app.run(debug=True)
