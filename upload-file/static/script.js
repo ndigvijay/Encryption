f0abcf578aacb754af8bbde9f07e7b75c98a6cdf
Nishanth <nishanthdmello2003@gmail.com>
2024-08-14 12:34:23 +0530

    deleted testing code

--- upload-file/static/script.js
+++
@@ -1,8 +0,0 @@
-// alert('hello')
-
-// but = document.getElementById('button1').addEventListener('click', ()=>{
-//     fetch()
-// })
-
-const file=document.getElementById("fileInput")
-console.log(file.files[0])
